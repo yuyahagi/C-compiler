9e1cc1fe692bdc3b18ba94a360b8eb308015bf37
Yu Yahagi <yuyahagi2@gmail.com>
2019-01-11 07:54:02 +0000

    Add and sub integers.

--- test.sh
+++ test.sh
@@ -11,12 +11,14 @@ try() {
     if [ "$actual" = "$expected" ]; then
         echo "$input => $actual"
     else
-        echo "$expected, but got $actual"
+        echo "FAIL: \"$input\""
+        echo "expected $expected but got $actual"
         exit 1
     fi
 }
 
 try 0 0
 try 42 42
+try 21 5+20-4
 
 echo OK
