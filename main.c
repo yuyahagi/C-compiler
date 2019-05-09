5d8f6510cc5b2ba7cf1846282740fce9f1208d39
Yu Yahagi <yuyahagi2@gmail.com>
2019-05-09 00:25:55 -0700

    Read from source file

--- main.c
+++ main.c
@@ -1,7 +1,33 @@
 #include <assert.h>
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
 #include "cc.h"
+
+char *read_file(char *path) {
+    FILE *pf = fopen(path, "r");
+    if (!pf) {
+        fprintf(stderr, "Could not open %s: %s\n", path, strerror(errno));
+        exit(1);
+    }
+
+    if (fseek(pf, 0, SEEK_END   ) == -1) {
+        fprintf(stderr, "Could not seek to the end of %s: %s\n", path, strerror(errno));
+        exit(1);
+    }
+    size_t filesize = ftell(pf);
+    if (fseek(pf, 0, SEEK_SET) == -1) {
+        fprintf(stderr, "Could not seek to the beginning of %s: %s\n", path, strerror(errno));
+        exit(1);
+    }
+
+    char *buf = malloc(filesize + 1);
+    fread(buf, filesize, 1, pf);
+    buf[filesize] = '\0';
+    fclose(pf);
+
+    return buf;
+}
 
 int main(int argc, char **argv) {
 #pragma GCC diagnostic ignored "-Wpointer-to-int-cast"
@@ -16,9 +42,11 @@ int main(int argc, char **argv) {
         runtest_type();
         return 0;
     }
+    
+    char *src = read_file(argv[1]);
 
     // Tokenize and parse to abstract syntax tree.
-    tokenize(argv[1]);
+    tokenize(src);
     program();
 
     printf(".intel_syntax noprefix\n");
